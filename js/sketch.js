79b8ceb93f2b2dec156d64196d315c98404dc5d8
Tom O'Brien <116882874+tomob1@users.noreply.github.com>
2022-11-29 10:58:17 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,40 @@
+var x, y, r;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    x = width/2;
+    y = height/2;
+    r = 200;
+    background(220);
 }
 
-function draw() {
-    background(220);
+if (time = < 0.2){
+    r = 100;
+} else r = 100{;
+}
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+
+function draw() {
+    
+
+
+    if(x < 0 - r/2){
+        x = height + r/2;
+    }
+
+    x--;
+
+    
+     if( x % 40 === 0 ){
+        stroke(10)
+     } else {
+        noStroke();
+     }
+
+     if( x % 10 === 0 ){
+        fill(random(0,255), random(0, 255), random(0,255));
+     }
+
+    ellipse(x, y, r);
 }
